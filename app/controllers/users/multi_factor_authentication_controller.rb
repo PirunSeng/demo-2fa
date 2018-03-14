7262d1ea35e4de326bdcc5c62f0b4c0bf04ea541
Pirun Seng <sengpirun.rain@gmail.com>
2018-03-14 17:34:08 +0700

    Try implementing another way of verifying 2fa code

--- app/controllers/users/multi_factor_authentication_controller.rb
+++ app/controllers/users/multi_factor_authentication_controller.rb
@@ -1,21 +1,45 @@
 class Users::MultiFactorAuthenticationController < ApplicationController
   before_action :set_user
 
-  def verify_enable
-    if current_user == @user &&
-       current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
-       current_user.otp_required_for_login = true
-       current_user.save!
+  # def verify_enable
+  #   if current_user == @user &&
+  #      current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
+  #      current_user.otp_required_for_login = true
+  #      current_user.save!
+  #
+  #      redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
+  #   else
+  #     redirect_to edit_user_registration_path, alert: t('.two_factor_authentication_could_not_be_enabled')
+  #   end
+  # end
 
-       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
+  def verify_enable
+    totp = ROTP::TOTP.new(current_user.otp_secret)
+
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
+      current_user.otp_required_for_login = true
+      current_user.save!
+      redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
     else
       redirect_to edit_user_registration_path, alert: t('.two_factor_authentication_could_not_be_enabled')
     end
   end
 
+  # def verify_disabled
+  #   if current_user == @user &&
+  #      current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
+  #     current_user.otp_required_for_login = false
+  #     current_user.save!
+  #     redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_disabled')
+  #   else
+  #     redirect_to edit_user_registration_path, alert: t('.two_factor_authentication_could_not_be_disabled')
+  #   end
+  # end
+
   def verify_disabled
-    if current_user == @user &&
-       current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
+    totp = ROTP::TOTP.new(current_user.otp_secret)
+
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
       current_user.otp_required_for_login = false
       current_user.save!
       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_disabled')
